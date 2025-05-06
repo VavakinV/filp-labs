5d7ca9aec6b06c85c500696f9efae2f7c555e74a
VavakinV <vladvavakin@gmail.com>
2025-05-06 23:55:58 +0300

    ИЗ1

---
+++ f#/IZ1/IZFunctions.fs
@@ -0,0 +1,31 @@
+module IZFunctions
+
+let modulo = 100000I
+let global_limit = 1000000000000I
+
+let rec shrink_number n =
+    if n % modulo = 0I then
+        let newN = n / 5I
+        shrink_number newN
+    else n
+
+let rec remove_tens n result =
+    match n % 5I = 0I with
+    | false -> n, result
+    | true -> 
+        let newResult = result / 2I
+        let newN = n / 5I
+        remove_tens newN newResult
+
+let five_digits_of_fact n =
+    let n = shrink_number n
+    let rec factfunc n acc limit =
+        match n > limit with
+        | true -> acc % modulo
+        | false ->
+            let current = n
+            let newN, newAcc = remove_tens n acc
+            let updatedAcc = (newAcc * (newN % modulo)) % global_limit
+            let nextN = n + 1I
+            factfunc nextN updatedAcc limit
+    factfunc 1I 1I n
---
+++ f#/IZ1/Program.fs
@@ -0,0 +1,6 @@
+﻿open IZFunctions
+
+[<EntryPoint>]
+let main argv =
+    System.Console.Write(five_digits_of_fact(1000000000000I))
+    0
